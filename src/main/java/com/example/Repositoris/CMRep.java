99ac23a15b83bec8ae2e4a180c7a98f955ff5377
pols28 <pschwinz18@jaumebalmes.net>
2022-11-09 17:52:04 +0100

    commit3

--- src/main/java/com/example/Repositoris/CMRep.java
+++ src/main/java/com/example/Repositoris/CMRep.java
@@ -1,16 +1,11 @@
 
 package com.example.Repositoris;
 import com.example.sbrealcions.CourseMaterial;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+
 @Repository
 public interface CMRep extends CrudRepository<CourseMaterial, Long> {
 
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,15 +1,14 @@
-
 INSERT INTO COURSE (TITLE) VALUES ('curs 1');
 INSERT INTO COURSE (TITLE) VALUES ('curs 2');
 INSERT INTO COURSE (TITLE) VALUES ('curs 3');
 INSERT INTO COURSE (TITLE) VALUES ('curs 4');
 INSERT INTO COURSE (TITLE) VALUES ('curs 5');
 
-INSERT INTO STUDENT (lastName, firstName, birthDate, wantsNewsletter) VALUES ('Joan', 'Petit', '2000-02-16', 'true');
-INSERT INTO STUDENT (lastName, firstName, birthDate, wantsNewsletter) VALUES ('Joanet', 'Petito', '2000-02-16', 'true');
-INSERT INTO STUDENT (lastName, firstName, birthDate, wantsNewsletter) VALUES ('Joana', 'Petita', '2000-02-16', 'false');
-INSERT INTO STUDENT (lastName, firstName, birthDate, wantsNewsletter) VALUES ('Joanito', 'Pepito', '2000-02-16', 'true');
-INSERT INTO STUDENT (lastName, firstName, birthDate, wantsNewsletter) VALUES ('John', 'Samll', '2000-02-16', 'false');
+INSERT INTO STUDENT (LAST_NAME, FIRST_NAME, BIRTH_DATE, WANTS_NEWSLETTER) VALUES ('Joan', 'Petit', '2000-02-16', 'true');
+INSERT INTO STUDENT (LAST_NAME, FIRST_NAME, BIRTH_DATE, WANTS_NEWSLETTER) VALUES ('Joanet', 'Petito', '2000-02-16', 'true');
+INSERT INTO STUDENT (LAST_NAME, FIRST_NAME, BIRTH_DATE, WANTS_NEWSLETTER) VALUES ('Joana', 'Petita', '2000-02-16', 'false');
+INSERT INTO STUDENT (LAST_NAME, FIRST_NAME, BIRTH_DATE, WANTS_NEWSLETTER) VALUES ('Joanito', 'Pepito', '2000-02-16', 'true');
+INSERT INTO STUDENT (LAST_NAME, FIRST_NAME, BIRTH_DATE, WANTS_NEWSLETTER) VALUES ('John', 'Samll', '2000-02-16', 'false');
 
 INSERT INTO COURSE_MATERIAL (URL) VALUES ('yava.com');
 INSERT INTO COURSE_MATERIAL (URL) VALUES ('java.com');
